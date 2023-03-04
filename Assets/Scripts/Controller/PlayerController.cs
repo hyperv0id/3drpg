284c600837855f9e95ae42f12d6981c9e010f0e6
jcheng <20722003@bjtu.edu.cn>
2023-03-04 11:38:11 +0800

    add animation, use blend tree

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -6,18 +6,27 @@ using UnityEngine.AI;
 public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
-
+    private Animator anim;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
     }
     private void Start()
     {
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
     }
+    private void Update()
+    {
+        ChangeAnimation();
+    }
     public void MoveToTarget(Vector3 target)
     {
         agent.destination = target;
+    }
+    private void ChangeAnimation()
+    {
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 }
